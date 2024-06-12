475eb39244993a9951efb6dc93944ccc3571b3d9
unknown <geral.johnny@gmail.com>
2024-06-11 21:38:34 -0300

    Inicio Projeto

---
+++ src/basic/ProjetoFinal.java
@@ -0,0 +1,43 @@
+package basic;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ProjetoFinal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProjetoFinal window = new ProjetoFinal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProjetoFinal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
